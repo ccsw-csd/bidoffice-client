995e0c042837f04331eb1293089930d14771844a
CORP\olagoang <oscar.lago-angel-external@capgemini.com>
2022-08-11 12:00:58 +0200

    BO-06 Borrando variables innecesarias

--- src/app/admin/views/file-type-edit/file-type-edit.component.spec.ts
+++ src/app/admin/views/file-type-edit/file-type-edit.component.spec.ts
@@ -8,30 +8,9 @@ describe('FileTypeEditComponent', () => {
     let mockDynamicDialogRef: any;
     let mockDynamicDialogConfig: any;
     let mockFileTypeService: any;
-  
-    let FILETYPE_ITEM: FileType[];
-    let FILETYPE_ITEM_EDITED: FileType[];
-    let FILETYPE_ITEM_SAVED: FileType[];
 
     beforeEach(() => {
-      FILETYPE_ITEM = [
-        new FileType ({id:1,name:"test",priority:2}),
-        new FileType({id:2,name:"test2",priority:3}),
-        new FileType({id:3,name:"tes3t",priority:1}),
-      ]
-      FILETYPE_ITEM_EDITED = [
-        new FileType ({id:1,name:"test",priority:2}),
-        new FileType({id:2,name:"test2",priority:3}),
-        new FileType({id:3,name:"New test",priority:1}),
-      ]
-      FILETYPE_ITEM_SAVED = [
-        new FileType ({id:1,name:"test",priority:2}),
-        new FileType({id:2,name:"test2",priority:3}),
-        new FileType({id:3,name:"tes3t",priority:1}),
-        new FileType({id:3,name:"New saved test",priority:1}),
-      ]
       
-  
       mockFileTypeService = jasmine.createSpyObj(['saveFileType','getFileTypes']);
       
       fileTypeEditComponent = new FileTypeEditComponent(mockDynamicDialogRef,mockDynamicDialogConfig,mockFileTypeService);
@@ -40,38 +19,38 @@ describe('FileTypeEditComponent', () => {
   
       it("editFileTypeShouldEdit",()=>{
         let editedFileType = new FileType({id:3,name:"New test",priority:1})
-          mockFileTypeService.saveFileType.and.returnValue(of(true))
-          fileTypeEditComponent.onSave(editedFileType)
-          expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
-          expect(fileTypeEditComponent.exceptionFail).toEqual(false)
-          expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
+        mockFileTypeService.saveFileType.and.returnValue(of(true))
+        fileTypeEditComponent.onSave(editedFileType)
+        expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
+        expect(fileTypeEditComponent.exceptionFail).toEqual(false)
+        expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
       })
   
       it("editFileTypeWrongNameShouldNotEdit",()=>{
         let editedFileType = new FileType({id:3,name:"New test",priority:3})
-          mockFileTypeService.saveFileType.and.returnValue(throwError(() => {409}))
-          fileTypeEditComponent.onSave(editedFileType)
-          expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
-          expect(fileTypeEditComponent.exceptionFail).toEqual(true)
-          expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
+        mockFileTypeService.saveFileType.and.returnValue(throwError(() => {409}))
+        fileTypeEditComponent.onSave(editedFileType)
+        expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
+        expect(fileTypeEditComponent.exceptionFail).toEqual(true)
+        expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
       })
 
       it("editFileTypeWrongPriorityShouldNotEdit",()=>{
         let editedFileType = new FileType({id:3,name:"test1",priority:1})
-          mockFileTypeService.saveFileType.and.returnValue(throwError(() => {409}))
-          fileTypeEditComponent.onSave(editedFileType)
-          expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
-          expect(fileTypeEditComponent.exceptionFail).toEqual(true)
-          expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
+        mockFileTypeService.saveFileType.and.returnValue(throwError(() => {409}))
+        fileTypeEditComponent.onSave(editedFileType)
+        expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
+        expect(fileTypeEditComponent.exceptionFail).toEqual(true)
+        expect(fileTypeEditComponent.exceptionMissing).toEqual(false)
       })
 
       it("editFileTypeEmptyFieldsShouldNotEdit",()=>{
         let editedFileType = new FileType({})
-          mockFileTypeService.saveFileType.and.returnValue(of(false))
-          fileTypeEditComponent.onSave(editedFileType)
-          expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
-          expect(fileTypeEditComponent.exceptionFail).toEqual(false)
-          expect(fileTypeEditComponent.exceptionMissing).toEqual(true)
+        mockFileTypeService.saveFileType.and.returnValue(of(false))
+        fileTypeEditComponent.onSave(editedFileType)
+        expect(fileTypeEditComponent.onSave(editedFileType)).not.toBeNull()
+        expect(fileTypeEditComponent.exceptionFail).toEqual(false)
+        expect(fileTypeEditComponent.exceptionMissing).toEqual(true)
       })
       
   });
