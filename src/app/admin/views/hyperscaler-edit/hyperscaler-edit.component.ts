28be61b6d297c39417934ad64db6d74ae4adf75e
TeviBR99 <92033034+TeviBR99@users.noreply.github.com>
2022-08-12 08:25:27 +0200

    Rango de prioridad
    Se ha establecido que la prioridad se encuentre solo en el rango de los números positivos

--- src/app/admin/views/hyperscaler-edit/hyperscaler-edit.component.ts
+++ src/app/admin/views/hyperscaler-edit/hyperscaler-edit.component.ts
@@ -13,6 +13,7 @@ import { HyperscalerService } from 'src/app/admin/services/hyperscaler.service';
 export class HyperscalerEditComponent implements OnInit {
   existsPriority: boolean
   fieldsNull: boolean
+  outOfRange: boolean
   elementHyperscaler: Hyperscaler
   elementBeforeChanges: Hyperscaler
 
@@ -27,20 +28,28 @@ export class HyperscalerEditComponent implements OnInit {
   }
 
   saveChanges(element: Hyperscaler){
-    if(element.name=="" || element.name==null || element.priority==null || element.priority < 1 ){
+    if(element.priority < 1 && element.priority!=null){
+      this.fieldsNull = false
+      this.existsPriority = false
+      this.outOfRange = true
+    }
+    else if(element.name=="" || element.name==null || element.priority==null ){
       this.fieldsNull = true
       this.existsPriority = false
+      this.outOfRange = false
     }
     else{
       this.hyperscalerService.saveHyperscaler(element).subscribe({
         next: () => { 
           this.existsPriority = false
           this.fieldsNull = false
+          this.outOfRange = false
           this.close()
         },
         error: () => {
           this.existsPriority = true;
           this.fieldsNull = false;
+          this.outOfRange = false
         }
       })
     }
