b2f9f4d9b3e33edf1d6494e20ca3ac4929a3b145
angeherr <aangello.mariluz@gmail.com>
2022-08-16 13:46:55 +0200

    se ha realizado lo siguiente: -Se ha desabilitado el boton de guardar cuando los inputs son invalidos -Se ha creado un toast indicando que se ha guardado correctamente(o no) un usuario -Se ha creado la operacion getRoles en userService para obtener los roles directamente de la bd.

--- src/app/admin/services/user.service.ts
+++ src/app/admin/services/user.service.ts
@@ -5,6 +5,7 @@ import { UserPage } from "../model/UserPage";
 import { Pageable } from "../../core/models/Pageable";
 import {environment} from "../../../environments/environment";
 import {User} from "../model/User";
+import {RoleClass} from "../model/RoleClass";
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,10 @@ export class UserService {
 
   saveUser(user: User): Observable<User>{
       return this.http.put<User>(environment.server + "/user", user);
+  }
+
+  getRoles(): Observable<RoleClass[]> {
+    return this.http.get<RoleClass[]>(environment.server + "/role/findAll");
   }
 
 }
--- src/app/admin/views/user-edit/user-edit.component.ts
+++ src/app/admin/views/user-edit/user-edit.component.ts
@@ -3,6 +3,7 @@ import { User } from "../../model/User";
 import { UserService } from '../../services/user.service';
 import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
 import { RoleClass } from "../../model/RoleClass";
+import { MessageService } from "primeng/api";
 
 @Component({
   selector: 'app-user-edit',
@@ -11,47 +12,56 @@ import { RoleClass } from "../../model/RoleClass";
 })
 export class UserEditComponent implements OnInit {
 
-  roles: RoleClass[];
+  roles: RoleClass[] = [];
 
   user: User;
-
-  showEmptyMessage: boolean;
 
   constructor(
     private userService: UserService,
     public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig
-  ) {
-    this.roles = [
-      {id: 1, name: 'Administrator'},
-      {id: 2, name: 'User'}
-    ]
-  }
+    public config: DynamicDialogConfig,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
     this.user = Object.assign({user: User}, this.config.data.user)
-
-    this.showEmptyMessage = false;
+    this.userService.getRoles().subscribe(rolesArray =>
+      this.roles = [...this.roles, ...rolesArray]
+    );
   }
 
   editUser(item: User) {
     if(item.username != "" && item.email != "") {
       this.userService.saveUser(item).subscribe({
         next: () => {
-          this.showEmptyMessage = false;
+          this.showSuccessMessage();
           this.onClose();
         },
         error: () => {
-          this.showEmptyMessage = false;
+          this.showErrorMessage();
         }
       });
-    } else {
-        this.showEmptyMessage = true;
     }
   }
 
   onClose(){
     this.ref.close();
+  }
+
+  showSuccessMessage(){
+    this.messageService.add({
+      key: 'userMessage',
+      severity:'success',
+      summary:'Éxito',
+      detail:'La operación se ha llevado a cabo correctamente'});
+  }
+
+  showErrorMessage(){
+    this.messageService.add({
+      key: 'userMessage',
+      severity:'error',
+      summary:'Error',
+      detail:'Error'});
   }
 
 }
