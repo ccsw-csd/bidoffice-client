33ce19f277348f2006800a9c7e4cba366a36c3dd
angeherr <aangello.mariluz@gmail.com>
2022-08-17 10:03:25 +0200

    tests arreglados

--- src/app/admin/views/user-edit/user-edit.component.spec.ts
+++ src/app/admin/views/user-edit/user-edit.component.spec.ts
@@ -3,31 +3,35 @@ import { UserEditComponent } from './user-edit.component';
 import { User } from "../../model/User";
 import { RoleClass } from "../../model/RoleClass";
 import { of } from "rxjs";
+import {MessageService} from "primeng/api";
 
 describe('UserEditComponent', () => {
   let userEditComponent;
   let mockUserService;
+  let mockRoleService;
   let ref;
   let dynamicDialogConfig;
+  let messageService;
+
 
   beforeEach(async () => {
     mockUserService = jasmine.createSpyObj(["saveUser"]);
+    mockRoleService = jasmine.createSpyObj(["getRoles"]);
     ref = jasmine.createSpyObj([""]);
-    dynamicDialogConfig = jasmine.createSpyObj([""])
-    userEditComponent = new UserEditComponent(mockUserService, ref, dynamicDialogConfig);
+    dynamicDialogConfig = jasmine.createSpyObj([""]);
+    messageService = jasmine.createSpyObj([""]);
+    userEditComponent = new UserEditComponent(mockUserService, mockRoleService, ref, dynamicDialogConfig, messageService);
   });
 
   it("editUserShouldUpdate", ()=>{
     let user = new User({id:11, username:"angeherr", email:"angeherr@capgemini.com", firstName:"Angello", lastName:"Herrera", role:{id: 2, name:"User"}});
     mockUserService.saveUser.and.returnValue(of(true));
     expect(userEditComponent.editUser(user)).not.toBeNull();
-    expect(userEditComponent.showEmptyMessage).toEqual(false)
   })
 
   it("editUserWithEmptyEmailFieldShouldThrowError", ()=>{
     let user = new User({id:11, username:"angeherr", email:"", firstName:"Angello", lastName:"Herrera", role:{id: 2, name:"User"}});
     expect(userEditComponent.editUser(user)).not.toBeNull();
-    expect(userEditComponent.showEmptyMessage).toEqual(true);
   })
 
 });
