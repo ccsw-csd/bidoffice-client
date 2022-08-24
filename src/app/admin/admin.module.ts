b30d111e224170c7afbc5280e814decb3645db5e
CORP\casanjua <carolina.sanjuan-hipolito-external@capgemini.com>
2022-08-24 12:24:08 +0200

    no message

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,30 +1,48 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileTypeListComponent } from './views/file-type-list/file-type-list.component';
-import { HyperscalerComponent } from './views/hyperscaler-list/hyperscaler.component';
+import { HyperscalerListComponent } from './views/hyperscaler-list/hyperscaler-list.component';
 import {TableModule} from 'primeng/table';
 import {ButtonModule} from 'primeng/button';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { MethodologyListComponent } from './views/methodology-list/methodology-list.component';
 import { UserListComponent } from './views/user-list/user-list.component';
+import { UserEditComponent } from "./views/user-edit/user-edit.component";
 import { PaginatorModule } from 'primeng/paginator';
 import { InputTextModule } from "primeng/inputtext";
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { HyperscalerEditComponent } from './views/hyperscaler-edit/hyperscaler-edit.component';
+import { TooltipModule } from 'primeng/tooltip';
+import { FileTypeEditComponent } from './views/file-type-edit/file-type-edit.component';
 import { ToastModule } from 'primeng/toast';
 import { TechnologyListComponent } from './views/technology-list/technology-list.component';
+import { OpportunityTypeListComponent } from './views/opportunity-type-list/opportunity-type-list.component';
+import { MethodologyEditComponent } from './views/methodology-edit/methodology-edit.component';
 import { OfferingListComponent } from './views/offering-list/offering-list.component';
 import { OfferingEditComponent } from './views/offering-edit/offering-edit.component';
+import { OpportunityTypeEditComponent } from './views/opportunity-type-edit/opportunity-type-edit.component';
+import { TechnologyEditComponent } from './views/technology-edit/technology-edit.component';
+import { ProjectTypeListComponent } from "./views/project-type-list/project-type-list.component";
+
 
 @NgModule({
   declarations: [
     FileTypeListComponent,
-    HyperscalerComponent,
+    HyperscalerListComponent,
     MethodologyListComponent,
     UserListComponent,
+    UserEditComponent,
+    HyperscalerEditComponent,
+    FileTypeEditComponent,
     TechnologyListComponent,
+    TechnologyEditComponent,
+    OpportunityTypeListComponent,
+    MethodologyEditComponent,
     OfferingListComponent,
-    OfferingEditComponent
+    OfferingEditComponent,
+    OpportunityTypeEditComponent,
+    ProjectTypeListComponent
   ],
- 
   imports: [
     CommonModule,
     TableModule,
@@ -32,7 +50,9 @@ import { OfferingEditComponent } from './views/offering-edit/offering-edit.compo
     ConfirmDialogModule,
     PaginatorModule,
     InputTextModule,
-    ToastModule
+    DynamicDialogModule,
+    TooltipModule,
+    ToastModule,
   ]
 })
 export class AdminModule { }
